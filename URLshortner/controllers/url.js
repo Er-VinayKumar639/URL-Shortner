72beda8efcae94f856628c9fc8aa292676fd890e
Er-VinayKumar639 <123vinaychaurasiya@gmail.com>
2024-04-13 23:31:05 +0530

    MongoDB connection established

--- URLshortner/controllers/url.js
+++ URLshortner/controllers/url.js
@@ -1,3 +1,21 @@
-async function handleGenerateShortURL(req, res){
+const shortid = require("shortid");
+const URL = require('../models/url');
+
+async function handleGenerateNewShortURL(req, res){
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error :'URL is required'});
+  const shortID= shortid(8);
+
+  await URL.create({
+    shortId : shortId,
+    redirectURL : body.url,
+    visitHistory : [],
+  });
   
+  return res.json({id : shortID });
 }
+
+module.exports = {
+  handleGenerateNewShortURL,
+};
+
