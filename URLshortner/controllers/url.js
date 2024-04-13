ccfdb83d323e75749078258930011e55aa340117
Er-VinayKumar639 <123vinaychaurasiya@gmail.com>
2024-04-13 22:06:02 +0530

    URL shortner structure has been generated

---
+++ URLshortner/controllers/url.js
@@ -0,0 +1,3 @@
+async function handleGenerateShortURL(req, res){
+  
+}
---
+++ URLshortner/routes/url.js
@@ -0,0 +1,5 @@
+const url = require("URL");
+const express = require("express");
+const router = require("express.Router()");
+
+router.post("./", )
